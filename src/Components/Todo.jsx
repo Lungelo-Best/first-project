87a0540d5f0934f2146a60f9182fe27fb4d7796a
Nomalungelo_Sithole <nsithole@ulwembubs.com>
2025-04-21 17:40:57 +0200

    fix: corrected import error + syntax mistake

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,31 +1,35 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './CSS/Todo.css'
+import TodoItems from './TodoItems';
 
 let count = 0;
 const Todo = () => {
-    const [todos, setTodos] = userState([]);
+    const [todos, setTodos] = useState([]);
     const inputRef = useRef(null);
 
     const add = () => {
-        setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}]);
+        setTodos([...todos,{no:count++, text:inputRef.current.value, display:""}]);
         inputRef.current.value = "";
     }
 
-    useEffect= (()=>{
+    useEffect(()=>{
         console.log(todos);
     },[todos])
+
 return (
     <div className='todo'>
         <div className="todo-header">Todo List</div>
         <div className="todo-add">
             <input ref={inputRef} type="text" placeholder='Add Your Task' className='todo-input'/>
-            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>  
+            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list">
-            
+        {todos.map((item,index)=>{
+            return <TodoItems key={index} no={item.no} display={item.display} text={item.text} />
+        })}
         </div>
     </div>
-)
+    )
 }
 
 export default Todo
