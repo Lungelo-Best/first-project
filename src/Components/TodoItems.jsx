5c7ef96036b78c0693e437acb88735f5a5815dca
Nomalungelo_Sithole <nsithole@ulwembubs.com>
2025-04-22 14:12:05 +0200

    feat: add logic assets + create localstorage

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -3,17 +3,39 @@ import tick from './Assets/tick.png'
 import not_tick from "./Assets/not_tick.png";
 import cross from "./Assets/cross.png";
 
-const TodoItems = ({no,
-    display,text}) => {
-        
+const TodoItems = ({no,display,text,setTodos}) => {
+
+    const deleteTodo = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todo)= todo.no!==no)
+    setTodos(data);
+    }
+
+    const toggle = () => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for (let  i= 0; i < data.length;i++)
+    {
+        if(data[i].no===no){
+            if(data[i].display===""){
+                data[i].display = "line-through"
+            }
+            else
+            {
+                data[i].display = ""
+            }
+            break;
+        }
+    }
+    setTodos(data);
+    }
+
 return (
     <div className='todoitems'>
-        <div className="todoitems-container">
-            <img src={not_tick} alt="" />
-            <img src={tick} alt="" />
-            <div className="todoitems-text">{text}</div>
+        <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
+        {display===""? <img src={not_tick} alt="" /> : <img src={tick} alt="" />} 
+        <div className="todoitems-text">{text}</div>
         </div>
-        <img src={cross} alt="" />
+        <img className='todoitems-crossicon' onClick={()=>{deleteTodo(no)}} src={cross} alt="" />
     </div>
     )
 }
