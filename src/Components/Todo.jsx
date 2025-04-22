9b41aeed9996de95ed8047ce5990a9aaa05f8226
Nomalungelo_Sithole <nsithole@ulwembubs.com>
2025-04-22 14:12:47 +0200

    feat: added local storage on browser

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -10,10 +10,19 @@ const Todo = () => {
     const add = () => {
         setTodos([...todos,{no:count++, text:inputRef.current.value, display:""}]);
         inputRef.current.value = "";
+        localStorage.setItem("todos_count", count)
     }
 
     useEffect(()=>{
+        setTodos(JSON.parse(localStorage.getItem("todos")));
+        count = localStorage.getItem("todos_count")
+    },[])
+
+    useEffect(()=>{
+        setTimeout(()=>{
         console.log(todos);
+        localStorage.setItem("todos",JSON.stringify(todos));
+        }, 100);
     },[todos])
 
 return (
@@ -25,7 +34,7 @@ return (
         </div>
         <div className="todo-list">
         {todos.map((item,index)=>{
-            return <TodoItems key={index} no={item.no} display={item.display} text={item.text} />
+            return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />
         })}
         </div>
     </div>
